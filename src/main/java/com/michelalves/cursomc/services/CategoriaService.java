6a9ef84ae6892f00767a17354a9e46f83728a56e
Michel Alves <snop_bsb@hotmail.com>
2018-08-20 15:30:54 -0300

    ajusd endPoint

--- src/main/java/com/michelalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/michelalves/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.michelalves.cursomc.domain.Categoria;
 import com.michelalves.cursomc.repositories.CategoriaRepository;
+import com.michelalves.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: "+ id + ", Tipo: "+ Categoria.class.getName());
+		}
 		return obj;
 	}
 }
