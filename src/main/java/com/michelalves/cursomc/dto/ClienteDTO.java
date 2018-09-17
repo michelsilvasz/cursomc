d833511a61932fa77c06cab9493760dc2aa60995
Michel Alves <snop_bsb@hotmail.com>
2018-09-17 16:26:12 -0300

    validacao email unico atualizacao

--- src/main/java/com/michelalves/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/michelalves/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.michelalves.cursomc.domain.Cliente;
+import com.michelalves.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
